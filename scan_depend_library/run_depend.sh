09f44e62b0052b1df4c2e3e15e0676f07491b454
opencolor111 <176214686@qq.com>
2019-12-31 14:42:52 +0800

    change mkdir

--- scan_depend_library/run_depend.sh
+++ scan_depend_library/run_depend.sh
@@ -2,6 +2,6 @@
 if ! rpm -qa|grep docker;then 
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl start docker && systemctl enable docker
 fi 
-mkdir /report
+test /report || mkdir /report
 docker run -v /report/:/app/templates/report --network host --rm registry.cn-chengdu.aliyuncs.com/system_centos/scan:v1 || docker run -v /report/:/app/templates/report --network host --rm registry.cn-chengdu.aliyuncs.com/system_centos/scan:v1 python manage.py runserver 0.0.0.0:8888
 

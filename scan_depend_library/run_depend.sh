3d713c3830442982460cf1a503e99664e176e40a
opencolor111 <176214686@qq.com>
2019-12-31 14:35:58 +0800

    add scan

---
+++ scan_depend_library/run_depend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if ! rpm -qa|grep docker;then 
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun && systemctl start docker && systemctl enable docker
+fi 
+mkdir /report
+docker run -v /report/:/app/templates/report --network host --rm registry.cn-chengdu.aliyuncs.com/system_centos/scan:v1 || docker run -v /report/:/app/templates/report --network host --rm registry.cn-chengdu.aliyuncs.com/system_centos/scan:v1 python manage.py runserver 0.0.0.0:8888
+
